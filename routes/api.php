55b877c5b8270c2889a6a183d57183d4492ce50a
Alfred Festo <alfredkalinga99@gmail.com>
2026-07-29 19:40:02 +0200

    fix: properly group authenticated routes in API routes file

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,7 @@ use Illuminate\Support\Facades\Route;
 Route::post('login', [AuthController::class, 'login'])->name('api.login');
 
 Route::middleware('auth:sanctum')->group(function (): void {
+    // Authenticated routes
     Route::get('posts', [PostController::class, 'index'])->name('api.posts.index');
     Route::get('posts/{slug}', [PostController::class, 'show'])->name('api.posts.show');
 
@@ -23,10 +24,11 @@ Route::middleware('auth:sanctum')->group(function (): void {
 
     Route::get('jobs', [JobController::class, 'index'])->name('api.jobs.index');
     Route::get('jobs/{slug}', [JobController::class, 'show'])->name('api.jobs.show');
-});
 
-Route::post('contact', [ContactController::class, 'store'])->name('api.contact.store');
-Route::post('calls', [CallController::class, 'store'])->name('api.calls.store');
-Route::post('project-request', [ProjectRequestController::class, 'store'])->name('api.project-request.store');
-Route::post('newsletter', [NewsletterController::class, 'store'])->name('api.newsletter.store');
-Route::post('unsubscribe', [NewsletterController::class, 'unsubscribe'])->name('api.newsletter.unsubscribe');
+    Route::post('contact', [ContactController::class, 'store'])->name('api.contact.store');
+    Route::post('calls', [CallController::class, 'store'])->name('api.calls.store');
+    Route::post('project-request', [ProjectRequestController::class, 'store'])->name('api.project-request.store');
+
+    Route::post('newsletter', [NewsletterController::class, 'store'])->name('api.newsletter.store');
+    Route::post('unsubscribe', [NewsletterController::class, 'unsubscribe'])->name('api.newsletter.unsubscribe');
+});
