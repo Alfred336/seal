e8fc7af90718049609a89aa9b2ebef8fc9fc9ed2
Michael Chapa <chapamichael00@gmail.com>
2026-08-20 15:04:01 +0300

    plan

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -18,6 +18,9 @@ return Application::configure(basePath: dirname(__DIR__))
     )
     ->withMiddleware(function (Middleware $middleware): void {
         $middleware->prepend(ForceHttps::class);
+        $middleware->append(
+            \Illuminate\Http\Middleware\HandleCors::class
+        );
 
         $middleware->alias([
             'role' => RoleMiddleware::class,
