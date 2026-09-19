cc8077686f4b6b8879aa68fe6f09e357f03b7041
Alfred Festo <alfredkalinga99@gmail.com>
2026-09-19 23:08:29 +0200

    feat: add supervisor configurations and installation script for queue and schedule workers

---
+++ supervisor/install.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+set -e
+
+DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+echo "Copying Supervisor configuration files to /etc/supervisor/conf.d/..."
+cp "${DIR}"/*.conf /etc/supervisor/conf.d/
+
+echo "Updating Supervisor..."
+supervisorctl reread
+supervisorctl update
+
+echo "Checking status of dev workers..."
+supervisorctl status dev-seal-queue:* dev-seal-schedule:*
